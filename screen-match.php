a7aa8ff073a6b27fb0c87863e148f5d06e451499
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-10-10 17:07:24 -0300

    operando sobre dados

--- screen-match.php
+++ screen-match.php
@@ -5,7 +5,7 @@ echo "Bem-vindo(a) ao screen match!
 
 $nomeFilme = "Top Gun - Maverick";
 $anoLancamento = 2022;
-$notaFilme = 8.8; //doble ou float
+$notaFilme = (9 + 6 + 8 + 7.5 + 5) / 5;
 $incluidoNoPlano = true;
 
-echo $nomeFilme;
+echo $notaFilme;
