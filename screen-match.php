01d0b9b329b666328c158132836e0162e69318fa
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-10-10 17:01:25 -0300

    add variaveis, e exibindo cno terminal com echo.

--- screen-match.php
+++ screen-match.php
@@ -1,4 +1,11 @@
 <?php
 
-echo "Bem-vindo(a) ao screen match!";
+echo "Bem-vindo(a) ao screen match!
+";
 
+$nomeFilme = "Top Gun - Maverick";
+$anoLancamento = 2022;
+$notaFilme = 8.8; //doble ou float
+$incluidoNoPlano = true;
+
+echo $nomeFilme;
