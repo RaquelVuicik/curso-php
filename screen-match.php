51b1c9b96fb82336ec7b5e77e62aa7586bf823f1
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-10-10 17:09:38 -0300

    soma de notas

--- screen-match.php
+++ screen-match.php
@@ -5,7 +5,8 @@ echo "Bem-vindo(a) ao screen match!
 
 $nomeFilme = "Top Gun - Maverick";
 $anoLancamento = 2022;
-$notaFilme = (9 + 6 + 8 + 7.5 + 5) / 5;
+$somaDeNotas = 9 + 6 + 8 + 7.5 + 5;
+$notaFilme = $somaDeNotas / 5;
 $incluidoNoPlano = true;
 
 echo $notaFilme;
