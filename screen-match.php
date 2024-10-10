5c9eb4771875edd1261f86520d2ed9760298756f
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-10-10 17:17:44 -0300

    operadores de acumulação

--- screen-match.php
+++ screen-match.php
@@ -5,7 +5,13 @@ echo "Bem-vindo(a) ao screen match!
 
 $nomeFilme = "Top Gun - Maverick";
 $anoLancamento = 2022;
-$somaDeNotas = 9 + 6 + 8 + 7.5 + 5;
+
+$somaDeNotas = 9;
+$somaDeNotas += 6;
+$somaDeNotas += 8;
+$somaDeNotas += 7.5;
+$somaDeNotas += 5;
+
 $notaFilme = $somaDeNotas / 5;
 $incluidoNoPlano = true;
 
