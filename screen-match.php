2b791e316e6b03e04f9f84bd085dc2cffcf89819
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-10-11 09:34:03 -0300

    utilizando a match expression

--- screen-match.php
+++ screen-match.php
@@ -3,6 +3,7 @@
 echo "Bem-vindo(a) ao screen match!\n";
 
 $nomeFilme = "Top Gun - Maverick";
+
 $anoLancamento = $argv[1] ?? 2022;
 
 $somaDeNotas = 9;
@@ -22,9 +23,17 @@ echo "Nota do filme: $notaFilme\n";
 echo "Ano de lançamento: $anoLancamento\n";
 
 if ($anoLancamento > 2022) {
-    echo "Esse filme é um lançamento";
+    echo "Esse filme é um lançamento\n";
 } elseif($anoLancamento > 2020 && $anoLancamento <= 2022) {
-    echo "Esse filme ainda é novo";
+    echo "Esse filme ainda é novo\n";
 } else {
-    echo "Esse filme não é um lançamento";
+    echo "Esse filme não é um lançamento\n";
 }
+
+$genero = match ($nomeFilme) {
+    "Top Gun - Maverick" => "ação",
+    "Thor: Ragnarok" => "super-herói",
+    "Se beber não case" => "comédia"
+};
+
+echo "O gênero do filme é: $genero";
