996810b6f2504d6268e3ef793f53c31910590481
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-10-22 11:09:29 -0300

    implementando uma interface

--- screen-match/src/Modelo/Episodio.php
+++ screen-match/src/Modelo/Episodio.php
@@ -1,12 +1,23 @@
 <?php
 
-class Episodio
+class Episodio implements Avaliavel
 {
+    private array $notas;
     public function __construct(
         public readonly Serie $serie,
         public readonly string $nome,
         public readonly int $numero,
     ) {
-        
+        $this->notas = [];
+    }
+
+    public function avalia(float $nota): void {
+        $this->notas[] = $nota;
+    }
+
+    public function media(): float {
+        $somaDasNotas = array_sum($this->notas);
+        $quantidadeNotas = count($this->notas);
+        return $somaDasNotas / $quantidadeNotas;
     }
 }
