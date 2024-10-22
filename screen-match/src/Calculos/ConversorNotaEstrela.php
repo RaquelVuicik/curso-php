36b04bb1a1ba597c56e87d25e3a415b59e1ef096
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-10-22 18:04:33 -0300

    lidando com erros e usando try-catch

--- screen-match/src/Calculos/ConversorNotaEstrela.php
+++ screen-match/src/Calculos/ConversorNotaEstrela.php
@@ -2,14 +2,20 @@
 
 namespace ScreenMatch\Calculos;
 
+use DivisionByZeroError;
 use ScreenMatch\Modelo\Avaliavel;
 
 class ConversorNotaEstrela
 {
     public function converte(Avaliavel $avaliavel): float
     {
-        $nota = $avaliavel->media();
+        try {
+            $nota = $avaliavel->media();
 
-        return round($nota) / 2;
+            return round($nota) / 2;
+        } catch (DivisionByZeroError $erro) {
+            echo $erro->getMessage();
+            return 0;
+        }
     }
 }
