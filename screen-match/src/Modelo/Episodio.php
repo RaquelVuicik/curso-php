f45efd2448c7e94b8b4c31b57456c64cbaf66975
Raquel Vuicik <raquelvuicik1@gmail.com>
2024-10-22 14:03:03 -0300

    utilizando traits

--- screen-match/src/Modelo/Episodio.php
+++ screen-match/src/Modelo/Episodio.php
@@ -2,22 +2,11 @@
 
 class Episodio implements Avaliavel
 {
-    private array $notas;
+    use ComAvaliacao;
     public function __construct(
         public readonly Serie $serie,
         public readonly string $nome,
         public readonly int $numero,
     ) {
-        $this->notas = [];
-    }
-
-    public function avalia(float $nota): void {
-        $this->notas[] = $nota;
-    }
-
-    public function media(): float {
-        $somaDasNotas = array_sum($this->notas);
-        $quantidadeNotas = count($this->notas);
-        return $somaDasNotas / $quantidadeNotas;
     }
 }
--- screen-match/src/Modelo/Titulo.php
+++ screen-match/src/Modelo/Titulo.php
@@ -2,25 +2,12 @@
 
 abstract class Titulo implements Avaliavel
 {
-    private array $notas;
+    use ComAvaliacao;
     public function __construct(
         public readonly string $nome,
         public readonly int $ano,
         public readonly Genero $genero,
-    )
-    {
-        $this->notas = [];
+    ) {
     }
-
-    public function avalia(float $nota): void {
-        $this->notas[] = $nota;
-    }
-
-    public function media(): float {
-        $somaDasNotas = array_sum($this->notas);
-        $quantidadeNotas = count($this->notas);
-        return $somaDasNotas / $quantidadeNotas;
-    }
-
     abstract public function duracaoEmMinutos(): int;
 }
